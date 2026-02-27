a7019b4c59b6e3b025acecd6aa1da438309bcf1d
AlanHusovic <alanhusovic755@gmail.com>
2026-02-27 13:58:27 +0100

    Modify states_of_america list and print it

---
+++ 32.py
@@ -0,0 +1,8 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia"]
+
+states_of_america[1] = "Pencilvania"
+
+states_of_america.extend(["Alanland", "Someone else land"])
+
+states_of_america.append("Nothing")
+print(states_of_america)
