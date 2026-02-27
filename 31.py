24f5b31586c6fd7a147b11a3ac29e22a82bb676e
AlanHusovic <alanhusovic755@gmail.com>
2026-02-27 13:42:36 +0100

    Add coin flip simulation in 31.py

---
+++ 31.py
@@ -0,0 +1,12 @@
+import random
+
+#import my_module
+#print(my_module.my_favorite_number)
+#random.randint(1, 10)
+#random_number_0_to_1 = random.random() * 10
+#random_float = random.uniform(1, 10)
+heads_or_tails = random.randint(0, 1)
+if heads_or_tails == 0:
+    print("Heads")
+else:
+    print("Tails")
